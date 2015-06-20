38bc8e4a5230d40a4e5c1168f954fa2fd7b0c357
katie Pekarek <katie_pekarek@yahoo.com>
2015-06-20 13:35:04 -0600

    answers cleared

--- bankAccount.js
+++ bankAccount.js
@@ -1,24 +1,5 @@
-var BankAccount = function(balance){
-  this.balance = balance;
+var BankAccount = function(){
+
 }
 
-BankAccount.prototype.deposit = function(transaction){
-    var that = this;
-    var args = Array.prototype.slice.call(arguments, 0);
-    args.forEach(function(elem){
-      if(elem.description == "deposit")
-      return that.balance += elem.ammount;
-    });
-
-};
-
-BankAccount.prototype.withdrawl = function(transaction){
-  if (transaction.description == "withdrawl"){
-    this.balance -= transaction.ammount;
-  }
-};
-
-BankAccount.prototype.printTotal = function(){
-  return this.balance;
-}
 module.exports = BankAccount;
