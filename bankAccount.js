db7947148c884ad7d518d135b3e6ff1ff71e6e72
Teddi Maull <teddi@picturetheday.com>
2015-06-20 14:56:45 -0600

    complete exercise

--- bankAccount.js
+++ bankAccount.js
@@ -1,5 +1,24 @@
-var BankAccount = function(){
-
+var BankAccount = function(balance){
+  this.balance = balance;
 }
 
+BankAccount.prototype.deposit = function (money) {
+  for (var i = 0; i < arguments.length; i++) {
+    if (arguments[i]["description"] === "deposit") {
+      this.balance += arguments[i]["ammount"];
+    }
+  }
+};
+
+BankAccount.prototype.withdrawl = function (money) {
+  for (var i = 0; i < arguments.length; i++) {
+    if (arguments[i]["description"] === "withdrawl") {
+      this.balance -= arguments[i]["ammount"];
+    }
+  }
+};
+
+BankAccount.prototype.printTotal = function () {
+  return this.balance;
+};
 module.exports = BankAccount;
